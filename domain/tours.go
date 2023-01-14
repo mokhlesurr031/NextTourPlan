676d06ef3ac98e07a83ad0f5f322accaf2a67ef0
Mokhlesur Mahin <mokhlesur.rahman@pathao.com>
2023-01-15 05:29:41 +0600

    meal,spot:add,list

--- domain/tours.go
+++ domain/tours.go
@@ -13,6 +13,14 @@ type TourSpots struct {
 	Description string `json:"description"`
 }
 
+type TourSpotsCriteria struct {
+	ID     *uint   `json:"id"`
+	TourID *uint   `json:"tour_id"`
+	Name   *string `json:"name"`
+	//Images      []*multipart.FileHeader `json:"images"`
+	Description *string `json:"description"`
+}
+
 type Meals struct {
 	ID        uint   `json:"id"`
 	TourID    uint   `json:"tour_id"`
@@ -23,44 +31,60 @@ type Meals struct {
 	Others    string `json:"others"`
 }
 
+type MealsCriteria struct {
+	ID        *uint   `json:"id"`
+	TourID    *uint   `json:"tour_id"`
+	BreakFast *string `json:"break_fast"`
+	Lunch     *string `json:"lunch"`
+	Dinner    *string `json:"dinner"`
+	Snacks    *string `json:"snacks"`
+	Others    *string `json:"others"`
+}
+
 type PlanForTour struct {
-	ID             uint   `json:"id"`
-	Name           string `json:"name"`
-	PickUpLocation string `json:"pick_up_location"`
-	Images         []byte `json:"images"`
-	//TourLocation   []TourSpots             `json:"tour_spots"`
-	//MealPlan       Meals                   `json:"meal_plan"`
-	Description string    `json:"description"`
-	DayCount    int       `json:"day_count"`
-	StartingAt  time.Time `json:"starting_at"`
-	CostPerHead float64   `json:"cost_per_head"`
-	CreatedBy   uint      `json:"created_by"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID             uint      `json:"id"`
+	Name           string    `json:"name"`
+	PickUpLocation string    `json:"pick_up_location"`
+	Description    string    `json:"description"`
+	DayCount       string    `json:"day_count"`
+	StartingAt     string    `json:"starting_at"`
+	CostPerHead    string    `json:"cost_per_head"`
+	CreatedBy      uint      `json:"created_by"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
 
 type PlanForTourCriteria struct {
-	ID             *uint   `json:"id"`
-	Name           *string `json:"name"`
-	PickUpLocation *string `json:"pick_up_location"`
-	//Images         []*byte `json:"images"`
-	//TourLocation   []TourSpots             `json:"tour_spots"`
-	//MealPlan       Meals                   `json:"meal_plan"`
-	Description *string    `json:"description"`
-	DayCount    *int       `json:"day_count"`
-	StartingAt  *time.Time `json:"starting_at"`
-	CostPerHead *float64   `json:"cost_per_head"`
-	CreatedBy   *uint      `json:"created_by"`
-	CreatedAt   *time.Time `json:"created_at"`
-	UpdatedAt   *time.Time `json:"updated_at"`
+	ID             *uint      `json:"id"`
+	Name           *string    `json:"name"`
+	PickUpLocation *string    `json:"pick_up_location"`
+	Description    *string    `json:"description"`
+	DayCount       *string    `json:"day_count"`
+	StartingAt     *string    `json:"starting_at"`
+	CostPerHead    *string    `json:"cost_per_head"`
+	CreatedBy      *uint      `json:"created_by"`
+	CreatedAt      *time.Time `json:"created_at"`
+	UpdatedAt      *time.Time `json:"updated_at"`
 }
 
 type PlanForTourRepository interface {
 	Post(ctx context.Context, ctr *PlanForTour) error
 	List(ctx context.Context, ctr *PlanForTourCriteria) ([]*PlanForTour, error)
+
+	Spots(ctx context.Context, ctr *TourSpots) error
+	SpotsList(ctx context.Context, ctr *TourSpotsCriteria) ([]*TourSpots, error)
+
+	Meals(ctx context.Context, ctr *Meals) error
+	MealsList(ctx context.Context, ctr *MealsCriteria) ([]*Meals, error)
 }
 
 type PlanForTourUseCase interface {
 	Post(ctx context.Context, ctr *PlanForTour) error
 	List(ctx context.Context, ctr *PlanForTourCriteria) ([]*PlanForTour, error)
+
+	Spots(ctx context.Context, ctr *TourSpots) error
+	SpotsList(ctx context.Context, ctr *TourSpotsCriteria) ([]*TourSpots, error)
+
+	Meals(ctx context.Context, ctr *Meals) error
+	MealsList(ctx context.Context, ctr *MealsCriteria) ([]*Meals, error)
 }
